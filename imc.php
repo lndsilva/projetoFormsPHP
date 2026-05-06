798685b9bd6076fd57ad7f38c05f5092650c79ea
lndsilva <lndsilva@hotmail.com>
2026-05-05 22:10:38 -0300

    trabalhando com formulários PHP

--- imc.php
+++ imc.php
@@ -7,20 +7,24 @@
 	<link rel="stylesheet" type="text/css" href="css/estilo.css">
 </head>
 <body>
-	
-	<h2>Tabela de classificação</h2>
-	
-	<img src="imagem/imc.png">
-	
-	<h1>Calculadora IMC</h1>
-	
-	<form action="calcular_imc.php" method="post">
-		<label for="altura">Sua altura:</label>
-		<input type="text" name="altura" id="altura" placeholder="Ex.: 1.65" >
-		<label for="peso">Seu peso:</label>
-		<input type="text" name="peso" id="peso" placeholder="Ex.: 85" >
-		<input type="submit" name="calcular" value="Calcular">
-	</form>
+
+	<div class="container">	
+		<h2>Tabela de classificação</h2>
+		
+		<img src="imagem/imc.png">
+		
+		<h1>Calculadora IMC</h1>
+		
+		<form action="calcular_imc.php" method="post">
+			<label for="nome">Seu nome:</label>
+			<input type="text" name="nome" id="nome" placeholder="Seu nome" required  maxlength="50" autocomplete="off" autofocus>
+			<label for="altura">Sua altura:</label>
+			<input type="text" name="altura" id="altura" placeholder="Ex.: 1.65" required autocomplete="off" maxlength="4">
+			<label for="peso">Seu peso:</label>
+			<input type="text" name="peso" id="peso" placeholder="Ex.: 85" required autocomplete="off" maxlength="3">
+			<input type="submit" name="calcular" value="Calcular">
+		</form>
+	</div>
 
 </body>
 </html>
