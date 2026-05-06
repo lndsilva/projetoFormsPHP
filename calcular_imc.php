91b072799c9e4974741e674146e06ddabbdc93c7
lndsilva <lndsilva@hotmail.com>
2026-05-05 21:57:01 -0300

    trabalhando com formulários PHP

--- calcular_imc.php
+++ calcular_imc.php
@@ -1,5 +1,7 @@
 <?php 
 
+	$nome = $_POST['nome'];
+
 	$peso = $_POST['peso'];
 
 	$altura = $_POST['altura'];
@@ -17,6 +19,25 @@
 	if ($imc >= 24.91 && $imc <= 25.00) {
 		$classificacao = "Sobrepeso";
 	}
+	if ($imc >= 25.01 && $imc <= 29.90) {
+		$classificacao = "Pré-obeso";
+	}
+	if ($imc >= 29.91 && $imc <= 34.90) {
+		$classificacao = "Obeso I";
+	}
+	if ($imc >= 34.91 && $imc <= 39.90) {
+		$classificacao = "Obeso II";
+	}
+	if ($imc >= 40.00) {
+		$classificacao = "Obeso III";
+	}
+
+	printf("Olá: $nome <br>");
+	printf("Peso inserido: %.2f <br>",$peso);
+	printf("Altura inserida: %.2f <br>", $altura);
+	printf("Sua classificação: $classificacao <br>");
+	printf("Seu IMC: %.2f", $imc);
+
 
 
 
